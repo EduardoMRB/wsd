11d6fa468c3de9dbb3e8642a0200b5e20fe12d73
Eduardo Borges <eduardomrb@gmail.com>
2014-11-12 13:41:34 -0300

    Sets twig to be the view layer. Closes #3
    TODO: implement default layout

--- index.php
+++ index.php
@@ -12,98 +12,98 @@ use WSD\Connection;
 use WSD\Config\ConfigContainer;
 
 $app = new \Slim\Slim(array(
-	'debug' => true,
-	'templates.path' => 'view',
+    'debug' => true,
+    'templates.path' => 'view',
+    'view' => new Slim\Views\Twig(),
 ));
 
 $container = new ConfigContainer(__DIR__ . '/config/config.yml');
 $con = new Connection($container);
 
 $app->get('/', function() use ($app, $con){
+    $apartamento = new Apartamento($con);
 
-	$apartamento = new Apartamento($con);
+    $data['apartamentos'] = $apartamento->selectData();
 
-	$data['apartamentos'] = $apartamento->selectData();
-
-	$app->render('index.inc.php', $data);
+    $app->render('index.html.twig', $data);
 
 })->name('home');
 
 $app->group('/apartamento', function() use ($app, $con){
 
-	$app->get('/new', function() use ($app, $con){
+    $app->get('/new', function() use ($app, $con){
 
-		$app->render('apartamentos/novo.inc.php');
+        $app->render('apartamentos/novo.inc.php');
 
-	})->name('new');
+    })->name('new');
 
-	$app->post('/new', function() use ($app, $con){
+    $app->post('/new', function() use ($app, $con){
 
-		$request = $app->request;
-		$apartamento = new Apartamento($con);
-		$apartamento->setMorador($request->post('morador'));
-		$apartamento->setMeses($request->post('meses'));
-		$apartamento->setNumero($request->post('numero'));
-		$apartamento->setSaldo($request->post('saldo'));
-		
-		if($apartamento->insertData($apartamento))
-			$app->flash('message', 'Dados inseridos com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao inserir os dados');
+        $request = $app->request;
+        $apartamento = new Apartamento($con);
+        $apartamento->setMorador($request->post('morador'));
+        $apartamento->setMeses($request->post('meses'));
+        $apartamento->setNumero($request->post('numero'));
+        $apartamento->setSaldo($request->post('saldo'));
 
-		$redirect = $app->urlFor('new');
+        if($apartamento->insertData($apartamento))
+            $app->flash('message', 'Dados inseridos com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao inserir os dados');
 
-		$app->redirect($redirect);
+        $redirect = $app->urlFor('new');
 
-	});
+        $app->redirect($redirect);
 
-	$app->get('/edit/:id', function($id=0) use ($app, $con){
+    });
 
-		$apartamento = new Apartamento($con);
+    $app->get('/edit/:id', function($id=0) use ($app, $con){
 
-		$data['apartamentos'] = $apartamento->editData($id);
-		$app->render('apartamentos/editar.inc.php', $data);
+        $apartamento = new Apartamento($con);
 
-	})->name('editApartamento');
+        $data['apartamentos'] = $apartamento->editData($id);
+        $app->render('apartamentos/editar.inc.php', $data);
 
-	$app->post('/edit/:id', function($id) use ($app, $con){
+    })->name('editApartamento');
 
-		$id = (int)$id;
+    $app->post('/edit/:id', function($id) use ($app, $con){
 
-		$request = $app->request;
+        $id = (int)$id;
 
-		$apartamento = new Apartamento($con);
-		$apartamento->setMorador($request->post('morador'));
-		$apartamento->setSaldo($request->post('saldo'));
-		$apartamento->setNumero($request->post('numero'));
-		$apartamento->setMeses($request->post('meses'));
-		$apartamento->setId($id);
+        $request = $app->request;
 
-		if($apartamento->updateData($apartamento))
-			$app->flash('message', 'Dados do morador atualizado com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao atualizar os dados');
+        $apartamento = new Apartamento($con);
+        $apartamento->setMorador($request->post('morador'));
+        $apartamento->setSaldo($request->post('saldo'));
+        $apartamento->setNumero($request->post('numero'));
+        $apartamento->setMeses($request->post('meses'));
+        $apartamento->setId($id);
 
-		$redirect = $app->urlFor('editApartamento', array('id' => $id));
+        if($apartamento->updateData($apartamento))
+            $app->flash('message', 'Dados do morador atualizado com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao atualizar os dados');
 
-		$app->redirect($redirect);
+        $redirect = $app->urlFor('editApartamento', array('id' => $id));
 
-	});
+        $app->redirect($redirect);
 
-	$app->get('/delete/:id', function($id=0) use ($app, $con){
+    });
 
-		$apartamento = new Apartamento($con);
+    $app->get('/delete/:id', function($id=0) use ($app, $con){
 
-		if($apartamento->deleteData($id))
-			$app->flash('message', 'Apartamento deletado com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao tentar deletar os dados');
+        $apartamento = new Apartamento($con);
 
-		$redirect = $app->urlFor('home');
+        if($apartamento->deleteData($id))
+            $app->flash('message', 'Apartamento deletado com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao tentar deletar os dados');
 
-		$app->redirect($redirect);
+        $redirect = $app->urlFor('home');
 
-	});
+        $app->redirect($redirect);
+
+    });
 
 });
 
@@ -111,134 +111,134 @@ $app->group('/apartamento', function() use ($app, $con){
 
 $app->group('/despesas' , function() use ($app, $con){
 
-	$app->get('/home', function() use ($app, $con){
+    $app->get('/home', function() use ($app, $con){
 
-		$mes = new Mes($con);
+        $mes = new Mes($con);
 
-		$data['despesas'] = $mes->selectData();
+        $data['despesas'] = $mes->selectData();
 
-		$app->render('despesas/index.inc.php', $data);
+        $app->render('despesas/index.inc.php', $data);
 
-	})->name('homeDespesas');
+    })->name('homeDespesas');
 
-	$app->get('/new', function() use ($app, $con){
+    $app->get('/new', function() use ($app, $con){
 
-		$app->render('despesas/novo.inc.php');
+        $app->render('despesas/novo.inc.php');
 
-	})->name('newDespesa');
+    })->name('newDespesa');
 
-	$app->post('/new', function() use ($app, $con){
+    $app->post('/new', function() use ($app, $con){
 
-		$request = $app->request;
-		$mes     = new Mes($con);
-		$mes->setVencimento($request->post('vencimento'));
-		$mes->setValorCondominio($request->post('valor'));
-		$idMesRef = $mes->insertData($mes);
+        $request = $app->request;
+        $mes     = new Mes($con);
+        $mes->setVencimento($request->post('vencimento'));
+        $mes->setValorCondominio($request->post('valor'));
+        $idMesRef = $mes->insertData($mes);
 
-		$despesa = new Despesa($con);
-		$despesa->setIdMesRef($idMesRef);
-		$despesa->setDescricao($request->post('descricao'));
-		$despesa->setValor($request->post('valorDespesa'));
+        $despesa = new Despesa($con);
+        $despesa->setIdMesRef($idMesRef);
+        $despesa->setDescricao($request->post('descricao'));
+        $despesa->setValor($request->post('valorDespesa'));
 
-		if($despesa->insertDespesa($despesa))
-			$app->flash('message', 'Dados inseridos com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao inserir os dados');
+        if($despesa->insertDespesa($despesa))
+            $app->flash('message', 'Dados inseridos com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao inserir os dados');
 
-		$redirect = $app->urlFor('newDespesa');
+        $redirect = $app->urlFor('newDespesa');
 
-		$app->redirect($redirect);
+        $app->redirect($redirect);
 
-	});
+    });
 
-	$app->get('/view/:id', function($id) use ($app, $con){
+    $app->get('/view/:id', function($id) use ($app, $con){
 
-		$id = (int)$id;
+        $id = (int)$id;
 
-		$despesa = new Despesa($con); 
+        $despesa = new Despesa($con); 
 
-		$data['despesas'] = $despesa->selectById($id);
+        $data['despesas'] = $despesa->selectById($id);
 
-		$app->render('despesas/report.inc.php', $data);
+        $app->render('despesas/report.inc.php', $data);
 
-	})->name('viewDespesa');
+    })->name('viewDespesa');
 
-	$app->get('/edit/:id', function($id=0) use ($app, $con){
+    $app->get('/edit/:id', function($id=0) use ($app, $con){
 
-		$despesa = new Despesa($con);
-		$mes 	 = new Mes($con);
+        $despesa = new Despesa($con);
+        $mes 	 = new Mes($con);
 
-		$data['despesas'] = $mes->selectById($id);
-		$data['allDespesas'] = $despesa->selectDespesaById($id);
-		$app->render('despesas/editar.inc.php', $data);
+        $data['despesas'] = $mes->selectById($id);
+        $data['allDespesas'] = $despesa->selectDespesaById($id);
+        $app->render('despesas/editar.inc.php', $data);
 
-	})->name('editDespesa');
+    })->name('editDespesa');
 
-	$app->post('/edit/:id', function($id) use ($app, $con){
+    $app->post('/edit/:id', function($id) use ($app, $con){
 
-		$id = (int)$id;
+        $id = (int)$id;
 
-		$request = $app->request;
+        $request = $app->request;
 
-		$mes     = new Mes($con);
-		$mes->setVencimento($request->post('vencimento'));
-		$mes->setValorCondominio($request->post('valor'));
-		$mes->setId($id);
-		$mes->updateData($mes);
+        $mes     = new Mes($con);
+        $mes->setVencimento($request->post('vencimento'));
+        $mes->setValorCondominio($request->post('valor'));
+        $mes->setId($id);
+        $mes->updateData($mes);
 
-		$despesa = new Despesa($con);
-		$despesa->setIdMesRef($id);
-		$despesa->setDescricao($request->post('descricao'));
-		$despesa->setValor($request->post('valorDespesa'));
-		$despesa->setIdDespesa($request->post('idDespesa'));
+        $despesa = new Despesa($con);
+        $despesa->setIdMesRef($id);
+        $despesa->setDescricao($request->post('descricao'));
+        $despesa->setValor($request->post('valorDespesa'));
+        $despesa->setIdDespesa($request->post('idDespesa'));
 
-		if($despesa->updateData($despesa))
-			$app->flash('message', 'Despesa atualizada com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao atualizar os dados');
+        if($despesa->updateData($despesa))
+            $app->flash('message', 'Despesa atualizada com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao atualizar os dados');
 
-		$redirect = $app->urlFor('editDespesa', array('id' => $id));
+        $redirect = $app->urlFor('editDespesa', array('id' => $id));
 
-		$app->redirect($redirect);
+        $app->redirect($redirect);
 
-	});
+    });
 
-	$app->get('/delete/:id', function($id=0) use ($app, $con){
+    $app->get('/delete/:id', function($id=0) use ($app, $con){
 
-		$despesa = new Despesa($con);
+        $despesa = new Despesa($con);
 
-		$mes = new Mes($con);
+        $mes = new Mes($con);
 
-		$mes->deleteData($id);
+        $mes->deleteData($id);
 
-		if($despesa->deleteData($id))
-			$app->flash('message', 'Despesa deletada com sucesso');
-		else
-			$app->flash('error', 'Houve um erro ao tentar deletar os dados');
+        if($despesa->deleteData($id))
+            $app->flash('message', 'Despesa deletada com sucesso');
+        else
+            $app->flash('error', 'Houve um erro ao tentar deletar os dados');
 
-		$redirect = $app->urlFor('homeDespesas');
+        $redirect = $app->urlFor('homeDespesas');
 
-		$app->redirect($redirect);
+        $app->redirect($redirect);
 
-	});
+    });
 
-	$app->get('/delete/single/:id', function($id=0) use ($app, $con){
+    $app->get('/delete/single/:id', function($id=0) use ($app, $con){
 
-		$despesa = new Despesa($con);
+        $despesa = new Despesa($con);
 
-		$despesa->deleteById($id);
+        $despesa->deleteById($id);
 
-	});
+    });
 
-	$app->get('/reporter/:id', function($id=0) use ($app, $con){
+    $app->get('/reporter/:id', function($id=0) use ($app, $con){
 
-		$despesa = new Despesa($con);
+        $despesa = new Despesa($con);
 
-		$data['despesas'] = $despesa->selectById($id);
+        $data['despesas'] = $despesa->selectById($id);
 
-		$app->render('despesas/report.inc.php', $data);
+        $app->render('despesas/report.inc.php', $data);
 
-	});
+    });
 
 });
 
